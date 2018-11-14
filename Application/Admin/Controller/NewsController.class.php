3dd7a56c755550039c002522d995589be0582377
qinshidong <qinshidong@faxiangkeji.com>
2018-11-14 17:48:59 +0800

    后台发布资讯,富文本框

--- Application/Admin/Controller/NewsController.class.php
+++ Application/Admin/Controller/NewsController.class.php
@@ -426,5 +426,55 @@ class NewsController extends AdminController {
             $this->redirect('recommenList');
         }
     }
+    //富文本框上传图片
+    public function uploadUediImg(){
 
+        date_default_timezone_set("Asia/chongqing");
+        error_reporting(E_ERROR);
+        header("Content-Type: text/html; charset=utf-8");
+
+        //TODO: 用户登录检测
+
+        /* 返回标准数据 */
+        $return  = array('status' => 1, 'info' => '上传成功', 'data' => '');
+        /* 调用文件上传组件上传文件 */
+        $Picture = D('Picture');
+        C('PICTURE_UPLOAD.maxSize',500*1024);   //上传的文件大小限制 (0-不做限制)
+        C('PICTURE_UPLOAD.exts','jpg,gif,png,jpeg');    //允许上传的文件后缀
+        C('PICTURE_UPLOAD.savePath','Picture/');   //设置上传子目录
+        $url = C('UPLOAD_SITEIMG_OSS.domain');  //拼接url头
+        $info = $Picture->upload(
+            $_FILES,
+            C('PICTURE_UPLOAD'),
+            C('PICTURE_UPLOAD_DRIVER'),
+            C('UPLOAD_SITEIMG_OSS'),
+            $url
+        ); //TODO:上传到远程服务器
+
+        /* 记录图片信息 */
+        if($info){
+            $return['status'] = 1;
+            $return = array_merge($info['fileList'], $return);
+//            p($return);
+        } else {
+            $return['status'] = 0;
+            $return['info']   = $Picture->getError();
+        }
+
+        /* 输出结果 */
+        $data['state'] = "SUCCESS";
+        $data['url'] = $return['path'];
+        $data['title'] = $return['path'];
+        $data['original'] = $return['path'];
+        /**
+         * {
+        "state": "SUCCESS",
+        "url": "upload/demo.jpg",
+        "title": "demo.jpg",
+        "original": "demo.jpg"
+        }
+         */
+        echo json_encode($data);
+        exit;
+    }
 }
